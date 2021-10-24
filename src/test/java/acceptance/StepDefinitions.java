08592e65a57bd5cd05f1ff79e59b8332ef537977
KatieLuna <69276423+KatieLuna@users.noreply.github.com>
2021-10-24 18:29:49 -0400

    Update StepDefinitions.java

--- src/test/java/acceptance/StepDefinitions.java
+++ src/test/java/acceptance/StepDefinitions.java
@@ -16,6 +16,8 @@ public class StepDefinitions {
     private String a;
     private String b;
     private String result;
+    
+    //Addition test
 
     @Given("^I have two numbers: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
@@ -26,6 +28,25 @@ public class StepDefinitions {
     @When("^the calculator sums them$")
     public void the_calculator_sums_them() throws Throwable {
         String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
+        result = restTemplate.getForObject(url, String.class);
+    }
+
+    @Then("^I receive (.*) as a result$")
+    public void i_receive_as_a_result(String expectedResult) throws Throwable {
+        assertEquals(expectedResult, result);
+    }
+    
+    //Division test    
+    
+    @Given("^I have two numbers: (.*) and (.*)$")
+    public void i_have_two_numbers(String a, String b) throws Throwable {
+        this.a = a;
+        this.b = b;
+    }
+
+    @When("^the calculator divides them$")
+    public void the_calculator_divides_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, a, b);
         result = restTemplate.getForObject(url, String.class);
     }
 
