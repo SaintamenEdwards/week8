667f185c3daaba64e3a9e7e8c439fed301f4a852
KatieLuna <69276423+KatieLuna@users.noreply.github.com>
2021-10-24 19:01:08 -0400

    Update StepDefinitions.java

--- src/test/java/acceptance/StepDefinitions.java
+++ src/test/java/acceptance/StepDefinitions.java
@@ -16,8 +16,6 @@ public class StepDefinitions {
     private String a;
     private String b;
     private String result;
-    
-    //Addition test
 
     @Given("^I have two numbers: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
@@ -34,30 +32,5 @@ public class StepDefinitions {
     @Then("^I receive (.*) as a result$")
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
-    }
-    
-    //Division test    
-    
-    private RestTemplate restTemplate2 = new RestTemplate();
-
-    private String a2;
-    private String b2;
-    private String result2;
-    
-    @Given("^I have two more numbers: (.*) and (.*)$")
-    public void i_have_two_more_numbers(String a2, String b2) throws Throwable {
-        this.a = a2;
-        this.b = b2;
-    }
-
-    @When("^the calculator divides them$")
-    public void the_calculator_divides_them() throws Throwable {
-        String url = String.format("%s/div?a=%s&b=%s", server, a2, b2);
-        result2 = restTemplate2.getForObject(url, String.class);
-    }
-
-    @Then("^I receive (.*) as a result$")
-    public void i_receive_as_another_result(String expectedResult2) throws Throwable {
-        assertEquals(expectedResult2, result2);
     }
 }
